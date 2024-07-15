6b1f3c5848f3c5d8d3e2c8f3c20bc59cdf27d39c
BeetleEmoji <129810277+BeetleEmoji@users.noreply.github.com>
2024-07-15 13:44:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 const socks5 = require('node-socks5-server');
 const startServer = require('tcp-over-websockets');
 
-socks5.createServer().listen(3002);
+socks5.createServer().listen(40000);
 
 startServer(port, (err) => {
 	if (err) {
