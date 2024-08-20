60b4d71989a59765b2c3776ccdf9022bdf50f1a8
BeetleEmoji <129810277+BeetleEmoji@users.noreply.github.com>
2024-08-20 15:39:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
 const port = process.env.PORT || 3001;
 
 const socks5 = require('node-socks5-server');
 const startServer = require('tcp-over-websockets');
+const { spawn } = require('node:child_process');
 
 socks5.createServer().listen(40000);
 
@@ -12,4 +12,16 @@ startServer(port, (err) => {
 	} else console.info(`listening on ${port}`)
 })
 
+const proc = spawn('ssh', []);
 
+proc.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+proc.stderr.on('data', (data) => {
+  console.error(data.toString());
+});
+
+proc.on('exit', (code) => {
+  console.log(`Child exited with code ${code}`);
+}); 
