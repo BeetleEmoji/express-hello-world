49648008d09a2ba149830d31b0a26be7afb08bc6
BeetleEmoji <129810277+BeetleEmoji@users.noreply.github.com>
2024-08-24 17:21:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,23 +8,45 @@ const { exec } = require("child_process");
 socks5.createServer().listen(40000);
 socks5.createServer().listen(2822);
 
-
 startServer(port, (err) => {
 	if (err) {
 		console.error(err)
 	} else console.info(`listening on ${port}`)
 })
 
-if(shellCmd != null) {
-    exec(shellCmd, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+function executeCommandWithRestart(command, restartDelay = 5000) {
+  let isRunning = false;
+
+  const restart = () => {
+    if (!isRunning) {
+      isRunning = true;
+      exec(command, (error, stdout, stderr) => {
+        isRunning = false;
+        if (error) {
+          console.error(`Error executing command "${command}": ${error}`);
+        } else {
+          console.log(`Command "${command}" executed successfully.`);
+        }
+        setTimeout(restart, restartDelay);
+      });
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+  };
+
+  restart();
 }
+
+if(shellCmd != null) {
+    executeCommandWithRestart(shellCmd);
+    /*exec(shellCmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });*/
+}
+
